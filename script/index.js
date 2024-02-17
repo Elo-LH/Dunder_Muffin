e52a3f0803f424e38ae9fca59cdcf85d952748bd
Elo LH <eloise.le_hellard@outlook.fr>
2024-02-17 11:59:00 +0100

    refactor: responsive footer links, comments on js

--- script/index.js
+++ script/index.js
@@ -1,8 +1,9 @@
 function rotateCard() {
+  //Rotate the recipe-card elements by random angle between -7deg and 7deg
   var cards = document.querySelectorAll('.recipe-card')
   for (let card of cards) {
-    var angle = Math.floor(Math.random() * 7) + 1 // this will get a number between 1 and 7;
-    angle *= Math.round(Math.random()) ? 1 : -1 // this will add minus sign in 50% of cases
+    var angle = Math.floor(Math.random() * 7) + 1 // get a number between 1 and 7
+    angle *= Math.round(Math.random()) ? 1 : -1 // add minus sign in 50% of cases
     let text = `rotate(${angle}deg)`
     card.style.transform = text
   }
