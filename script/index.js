3fff06592510e5b39b526b6a309e6b50cc31c75e
Elo LH <eloise.le_hellard@outlook.fr>
2024-02-19 10:56:09 +0100

    refactor: inside of contact form horizontal, better disposition

--- script/index.js
+++ script/index.js
@@ -9,15 +9,26 @@ console.log(recipes)
 function rotateCard() {
   //Rotate the recipe-card elements by random angle between -7deg and 7deg
   console.log('Entered rotateCard')
+  let form = false
   var cards = document.querySelectorAll('.recipe-card')
-  console.log(cards.length)
-  cards.length == 0 && (cards = document.querySelectorAll('input, textarea'))
-
+  if (cards.length == 0) {
+    cards = document.querySelectorAll('.form-card')
+    form = true
+  }
   for (let card of cards) {
     var angle = Math.floor(Math.random() * 7) + 1 // get a number between 1 and 7
     angle *= Math.round(Math.random()) ? 1 : -1 // add minus sign in 50% of cases
     let text = `rotate(${angle}deg)`
     card.style.transform = text
+    console.log('card is' + card)
+    if (form) {
+      let children = card.children
+      console.log(children)
+      for (let child of children) {
+        let correct = `rotate(${-angle}deg)`
+        child.style.transform = correct
+      }
+    }
   }
 }
 
